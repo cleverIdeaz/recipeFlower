99f56a43163b9aa54880af288af273f9f8b5a27f
Walter <167903018+cleverIdeaz@users.noreply.github.com>
2024-10-12 13:15:07 -0400

    serv

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect('mongodb+srv://nealandersontech:lbfVgSputI7qEgke@recipeflower.2aa68.mongodb.net/recipeDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB:', err);
 });
 
 const recipeSchema = new mongoose.Schema({
