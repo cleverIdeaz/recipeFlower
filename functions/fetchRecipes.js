71c2e2024e1cf43002b46e5e14446eb1533ac677
Walter <167903018+cleverIdeaz@users.noreply.github.com>
2024-10-14 22:45:17 -0400

    Update fetchRecipes.js

--- functions/fetchRecipes.js
+++ functions/fetchRecipes.js
@@ -25,12 +25,3 @@ exports.handler = async  (event, context) => {
   }
 };
 
-
-
-// getRecipes.js
-exports.handler = async function (event, context) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello from the function!" }),
-    };
-  };
