38319a3300a03b85448a3861903514af990e7392
Walter <167903018+cleverIdeaz@users.noreply.github.com>
2024-10-14 22:50:47 -0400

    Update fetchRecipes.js

--- functions/fetchRecipes.js
+++ functions/fetchRecipes.js
@@ -1,13 +1,13 @@
 const { MongoClient } = require("mongodb");
 
-exports.handler = async  (event, context) => {
+exports.handler = async (event, context) => {
   const uri = "mongodb+srv://nealandersontech:<db_password>@recipeflower.2aa68.mongodb.net/recipeflower?retryWrites=true&w=majority&appName=recipeflower"; // Update with your actual password
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
   try {
     await client.connect();
-    const database = client.db("RecipeFlower"); // Use your database name here
-    const recipes = database.collection("Recipes");
+    const database = client.db("recipeflower"); // Use your database name here
+    const recipes = database.collection("Recipes"); // Use the correct collection name
     const results = await recipes.find({}).toArray();
 
     return {
@@ -15,13 +15,12 @@ exports.handler = async  (event, context) => {
       body: JSON.stringify(results),
     };
   } catch (error) {
-    console.error('Error fetching recipess:', error); // Log the error for debugging
+    console.error('Error fetching recipes:', error); // Log the error for debugging
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Failed to fetch recipess." }),
+      body: JSON.stringify({ error: "Failed to fetch recipes!" }),
     };
   } finally {
     await client.close();
   }
 };
-
