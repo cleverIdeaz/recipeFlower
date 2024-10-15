6f306f10306f68d3f12d7cac7d7e7c3d1bab8c81
Walter <167903018+cleverIdeaz@users.noreply.github.com>
2024-10-14 21:42:22 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,34 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://nealandersontech:lbfVgSputI7qEgke@recipeflower.2aa68.mongodb.net/?retryWrites=true&w=majority&appName=recipeflower";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// above is example code for connecting to the database from mongodb website
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
